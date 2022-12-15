56b18e219b65b843543fe48431a8f681fbacda6d
Xabier Cantero López <xabier.cantero@deusto.es>
2022-12-15 08:04:09 +0000

    Different parameters for training and fforma

--- why_predictor/cli.py
+++ why_predictor/cli.py
@@ -6,7 +6,7 @@ import logging
 import os
 import sys
 from pathlib import Path
-from typing import Dict, List, Union
+from typing import Any, Dict, List, Union
 
 import argcomplete  # type: ignore
 import pandas as pd  # type: ignore
@@ -28,7 +28,7 @@ load_dotenv(dotenv_path=Path("./config.env"))
 class CustomFormatter(argparse.HelpFormatter):
     """Custom formatter to break lines in Help text preceded by #"""
 
-    def _split_lines(self, text, width):
+    def _split_lines(self, text: str, width: int) -> Any:
         if text.startswith("#"):
             return text[1:].splitlines()
         # this is the RawTextHelpFormatter._split_lines
@@ -38,7 +38,7 @@ class CustomFormatter(argparse.HelpFormatter):
 def generate_parser() -> argparse.ArgumentParser:
     """Generate parser for command line arguments"""
     parser = argparse.ArgumentParser(
-        prog="python -m why-predictor",
+        prog="python -m why_predictor",
         description="WHY Predictor",
         formatter_class=CustomFormatter,
     )
@@ -67,25 +67,6 @@ def generate_parser() -> argparse.ArgumentParser:
         default=os.getenv("DATASET_DIRNAME"),
         help="exact name of the directory containing the CSV files",
     )
-    models = os.getenv("MODELS")
-    parser.add_argument(
-        "--use-models",
-        dest="models",
-        choices=[f"{e.name}" for e in Models],
-        nargs="+",
-        type=str.upper,
-        default=json.loads(models) if models else "LR",
-        help="#Select what models to use:\n    "
-        + "\n    ".join([f"{e.name} ({e.value.name})" for e in Models]),
-    )
-    parser.add_argument(
-        "--error-type",
-        dest="error_type",
-        choices=["MAPE", "MAE", "RMSE", "SMAPE"],
-        type=str.upper,
-        default=os.getenv("ERROR_TYPE"),
-        help="metric to calculate the error",
-    )
     parser.add_argument(
         "--window-num-features",
         dest="num_features",
@@ -100,31 +81,75 @@ def generate_parser() -> argparse.ArgumentParser:
         default=os.getenv("NUM_PREDICTIONS"),
         help="num of hours used as predictions",
     )
-    generate_errors = parser.add_argument_group("Generate Errors")
+    generate_errors = parser.add_argument_group("Model training")
+    models = os.getenv("MODELS_TRAINING")
+    generate_errors.add_argument(
+        "--use-models-training",
+        dest="models_training",
+        choices=[f"{e.name}" for e in Models],
+        nargs="+",
+        type=str.upper,
+        default=json.loads(models) if models else "LR",
+        help="#Select what models to use:\n    "
+        + "\n    ".join([f"{e.name} ({e.value.name})" for e in Models]),
+    )
+    generate_errors.add_argument(
+        "--error-type-training",
+        dest="error_type_models",
+        choices=["MAPE", "MAE", "RMSE", "SMAPE"],
+        type=str.upper,
+        default=os.getenv("ERROR_TYPE_MODELS"),
+        help="metric to calculate the error",
+    )
     generate_errors.add_argument(
         "--percentage-csv-files-for-training-hyperparameters",
         dest="training_percentage_hyperparams",
         type=float,
-        default=os.getenv("TRAINING_PERCENTAGE_HYPERPARAMETERS"),
+        default=os.getenv("TRAINING_PERCENTAGE_MODELS"),
         help="Percentage of the CSV files that will be used for training",
     )
     generate_errors.add_argument(
         "--train-test-ratio-hyperparameters",
         dest="train_test_ratio_hyperparams",
         type=float,
-        default=os.getenv("TRAIN_TEST_RATIO_HYPERPARAMETERS"),
+        default=os.getenv("TRAIN_TEST_RATIO_MODELS"),
         help="ratio of samples used for training "
         + "(1 - this value will be used for testing)",
     )
-    generate_errors = parser.add_argument_group("Generate FFORMA")
-    generate_errors.add_argument(
+    generate_fforma = parser.add_argument_group("FFORMA training")
+    models_fforma = os.getenv("MODELS_FFORMA")
+    generate_fforma.add_argument(
+        "--use-models-fforma",
+        dest="models_fforma",
+        choices=[f"{e.name}" for e in Models],
+        nargs="+",
+        type=str.upper,
+        default=json.loads(models_fforma) if models_fforma else "LR",
+        help="#Select what models to use:\n    "
+        + "\n    ".join(
+            [
+                f"{e.name} ({e.value.name})"
+                for e in Models
+                if e.name.startswith("MULTI")
+            ]
+        ),
+    )
+    generate_fforma.add_argument(
+        "--error-type-fforma",
+        dest="error_type_fforma",
+        choices=["MAPE", "MAE", "RMSE", "SMAPE"],
+        type=str.upper,
+        default=os.getenv("ERROR_TYPE_FFORMA"),
+        help="metric to calculate the error",
+    )
+    generate_fforma.add_argument(
         "--percentage-csv-files-for-training-fforma",
         dest="training_percentage_fforma",
         type=float,
         default=os.getenv("TRAINING_PERCENTAGE_FFORMA"),
         help="Percentage of the CSV files that will be used for training",
     )
-    generate_errors.add_argument(
+    generate_fforma.add_argument(
         "--train-test-ratio-fforma",
         dest="train_test_ratio_fforma",
         type=float,
@@ -153,17 +178,13 @@ def select_hyperparameters(
         args.train_test_ratio_hyperparams,
     )
     # Calculate models
-    for model_name in args.models:
+    for model_name in args.models_training:
         Models[model_name].value(
-            train_features, train_output, ErrorType[args.error_type]
-        ).fit(
-            test_features,
-            test_output,
-            base_path="model-training"
-        )
+            train_features, train_output, ErrorType[args.error_type_models]
+        ).fit(test_features, test_output, base_path="model-training")
 
 
-def generate_fforma(
+def fforma_training(
     series: Dict[str, List[str]], args: argparse.Namespace
 ) -> pd.DataFrame:
     """Generate FFORMA ensemble"""
@@ -210,7 +231,7 @@ def _load_models_dict(
     test_output: pd.DataFrame,
 ) -> Dict[str, BasicModel]:
     models_dict = {}
-    for model_name in args.models:
+    for model_name in args.models_training:
         filename = f"model-training/hyperparameters/{model_name}.json"
         try:
             with open(filename, encoding="utf8") as fhyper:
@@ -221,7 +242,7 @@ def _load_models_dict(
         models_dict[model_name] = Models[model_name].value(
             train_features,
             train_output,
-            ErrorType[args.error_type],
+            ErrorType[args.error_type_fforma],
             hyperparameters,
         )
         models_dict[model_name].fit(
@@ -267,7 +288,7 @@ def main() -> None:
     # Execute select_fforma if mode is [generate-fforma or full]
     if args.mode != "generate-errors":
         logger.info("* Generating FFORMA...")
-        generate_fforma(series, args)
+        fforma_training(series, args)
 
 
 if __name__ == "__main__":
